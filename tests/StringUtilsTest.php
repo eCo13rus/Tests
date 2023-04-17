0b16d136f4a729a6e9f128f439d2208e7cac277b
Ilya Shirokov <asd.mat@mail.ru>
2023-04-17 15:55:16 +0300

    Update

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once __DIR__ . "/../src/StringUtils.php";
+require_once "vendor/autoload.php";
+
+use Webmozart\Assert\Assert;
+use function StringUtils\capitalize;
 
 if (StringUtils\capitalize('hello') !== 'Hello') {
     throw new \Exception('Функция работает неверно!');
@@ -9,6 +13,10 @@ if (StringUtils\capitalize('hello') !== 'Hello') {
 if (StringUtils\capitalize('') !== '') {
     throw new \Exception('Функция работает неверно!');
 }
+
+
+Assert::eq(capitalize('hello'), 'Hello');
+Assert::notEq(true, false);
 
 echo 'Все тесты пройдены!';
 
